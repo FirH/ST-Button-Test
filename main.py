4ae84b279e60907e31b5d44751112f917fac6a4f
Firhan Imam Haekal <firhanimamhaekal@gmail.com>
2022-11-05 08:40:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.columns((1,2,3))
+col1, col2 = st.columns((1,5))
+with col1 :
+    avs = st.button('Test')
+with col2 :
+    krs = st.button('Test2')
+
+if(avs) :
+    st.write('Mampus')
+if(krs) :
+    st.write('Haha')
